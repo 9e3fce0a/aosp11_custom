3d831bde8ddd181a8f19b268a6cc727cb9faf341
9e3fce0a <9e3fce0a@users.noreply.github.com>
2021-08-10 03:56:10 +0000

    Initial commit - migrated from aosp10_custom

---
+++ 00002-custom-boot-animation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#BOOT_ANIMATION="https://github.com/RattlesnakeOS/example_patch_shellscript/raw/master/bootanimation.zip"
+BOOT_ANIMATION="https://github.com/9e3fce0a/aosp10_custom/raw/master/bootanimation.zip"
+mkdir -p $BUILD_DIR/branding
+echo "Downloading boot animation $BOOT_ANIMATION"
+wget -O $BUILD_DIR/branding/bootanimation.zip $BOOT_ANIMATION
+sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/target/product/handheld_system.mk
